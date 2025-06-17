5735df4214bbdb2694d4121dd314e8e73fa38d08
Ayush Soni <73236440+ayushsoni77@users.noreply.github.com>
2025-06-17 12:12:18 +0530

    Fix React Application Setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './styles/components.css'
-import './styles/sections.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
